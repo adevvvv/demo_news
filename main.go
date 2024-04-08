bff464a28688613ebb98d2baba2bb63057ac0265
adevvvv <adevvv@bk.ru>
2024-04-08 12:58:41 +0300

    feat: добавить роут /search

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
-package demo_news
+package main
 
 import (
-	"github.com/joho/godotenv"
+	"fmt"
 	"html/template"
-	"log"
 	"net/http"
+	"net/url"
 	"os"
 )
 
@@ -14,21 +14,38 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tpl.Execute(w, nil)
 }
 
-func main() {
-	err := godotenv.Load()
+func searchHandler(w http.ResponseWriter, r *http.Request) {
+	u, err := url.Parse(r.URL.String())
 	if err != nil {
-		log.Println("Error loading .env file")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal server error"))
+		return
 	}
 
+	params := u.Query()
+	searchKey := params.Get("q")
+	page := params.Get("page")
+	if page == "" {
+		page = "1"
+	}
+
+	fmt.Println("Search Query is: ", searchKey)
+	fmt.Println("Results page is: ", page)
+}
+
+func main() {
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8000"
+		port = "3000"
 	}
 
-	fs := http.FileServer(http.Dir("assets"))
-
 	mux := http.NewServeMux()
+
+	fs := http.FileServer(http.Dir("assets"))
 	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	// Add the next line
+	mux.HandleFunc("/search", searchHandler)
 	mux.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":"+port, mux)
 }
