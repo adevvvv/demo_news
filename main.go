d2c2b6ef23c9a065453f8a20bc2a746eaac34be1
adevvvv <adevvv@bk.ru>
2024-04-08 12:59:50 +0300

    feat: создать модель данных

--- main.go
+++ main.go
@@ -6,7 +6,22 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"time"
 )
+
+type Article struct {
+	Source struct {
+		ID   interface{} `json:"id"`
+		Name string      `json:"name"`
+	} `json:"source"`
+	Author      string    `json:"author"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	URL         string    `json:"url"`
+	URLToImage  string    `json:"urlToImage"`
+	PublishedAt time.Time `json:"publishedAt"`
+	Content     string    `json:"content"`
+}
 
 var tpl = template.Must(template.ParseFiles("index.html"))
 
