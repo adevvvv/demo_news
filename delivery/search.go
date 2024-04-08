919dd856373bfb5431693c0c8aac68f5515c6711
adevvvv <adevvv@bk.ru>
2024-04-08 16:50:14 +0300

    refactor: изменить структуру

---
+++ delivery/search.go
@@ -0,0 +1,96 @@
+package delivery
+
+import (
+	"bytes"
+	"demo_news/model"
+	"html/template"
+	"math"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+var tpl = template.Must(template.ParseFiles("index.html"))
+
+type Search struct {
+	Query      string
+	NextPage   int
+	TotalPages int
+	Results    *news.Results
+}
+
+func (s *Search) IsLastPage() bool {
+	return s.NextPage >= s.TotalPages
+}
+
+func (s *Search) CurrentPage() int {
+	if s.NextPage == 1 {
+		return s.NextPage
+	}
+
+	return s.NextPage - 1
+}
+
+func (s *Search) PreviousPage() int {
+	return s.CurrentPage() - 1
+}
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	buf := &bytes.Buffer{}
+	err := tpl.Execute(buf, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	buf.WriteTo(w)
+}
+
+func SearchHandler(newsapi *news.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		u, err := url.Parse(r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		params := u.Query()
+		searchQuery := params.Get("q")
+		page := params.Get("page")
+		if page == "" {
+			page = "1"
+		}
+
+		results, err := newsapi.FetchEverything(searchQuery, page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		nextPage, err := strconv.Atoi(page)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		search := &Search{
+			Query:      searchQuery,
+			NextPage:   nextPage,
+			TotalPages: int(math.Ceil(float64(results.TotalResults) / float64(newsapi.PageSize))),
+			Results:    results,
+		}
+
+		if ok := !search.IsLastPage(); ok {
+			search.NextPage++
+		}
+
+		buf := &bytes.Buffer{}
+		err = tpl.Execute(buf, search)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		buf.WriteTo(w)
+	}
+}
--- main.go
+++ main.go
@@ -1,104 +1,14 @@
 package main
 
 import (
-	"bytes"
-	"demo_news/news"
-	"html/template"
-	"log"
-	"math"
-	"net/http"
-	"net/url"
-	"os"
-	"strconv"
-	"time"
-
+	handler "demo_news/delivery"
+	news "demo_news/model"
 	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
-
-var tpl = template.Must(template.ParseFiles("index.html"))
-
-type Search struct {
-	Query      string
-	NextPage   int
-	TotalPages int
-	Results    *news.Results
-}
-
-func (s *Search) IsLastPage() bool {
-	return s.NextPage >= s.TotalPages
-}
-
-func (s *Search) CurrentPage() int {
-	if s.NextPage == 1 {
-		return s.NextPage
-	}
-
-	return s.NextPage - 1
-}
-
-func (s *Search) PreviousPage() int {
-	return s.CurrentPage() - 1
-}
-
-func indexHandler(w http.ResponseWriter, r *http.Request) {
-	buf := &bytes.Buffer{}
-	err := tpl.Execute(buf, nil)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	buf.WriteTo(w)
-}
-
-func searchHandler(newsapi *news.Client) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		u, err := url.Parse(r.URL.String())
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		params := u.Query()
-		searchQuery := params.Get("q")
-		page := params.Get("page")
-		if page == "" {
-			page = "1"
-		}
-
-		results, err := newsapi.FetchEverything(searchQuery, page)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		nextPage, err := strconv.Atoi(page)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		search := &Search{
-			Query:      searchQuery,
-			NextPage:   nextPage,
-			TotalPages: int(math.Ceil(float64(results.TotalResults) / float64(newsapi.PageSize))),
-			Results:    results,
-		}
-
-		if ok := !search.IsLastPage(); ok {
-			search.NextPage++
-		}
-
-		buf := &bytes.Buffer{}
-		err = tpl.Execute(buf, search)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		buf.WriteTo(w)
-	}
-}
 
 func main() {
 	err := godotenv.Load()
@@ -123,7 +33,7 @@ func main() {
 
 	mux := http.NewServeMux()
 	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
-	mux.HandleFunc("/search", searchHandler(newsapi))
-	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/search", handler.SearchHandler(newsapi))
+	mux.HandleFunc("/", handler.IndexHandler)
 	http.ListenAndServe(":"+port, mux)
 }
